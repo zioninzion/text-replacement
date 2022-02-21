42f90185b2ba2215c7169d14b4b1cb3a948be134
Zion Park <zionpark@Zions-MBP.wifi.gsu.edu>
2022-02-21 14:20:24 -0500

    init

---
+++ atext/csv_to_plist.py
@@ -0,0 +1,9 @@
+result = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+result += "\n<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">"
+result += "\n<plist version=\"1.0\">"
+result += "\n<array>\n"
+result_end = "</array>\n</plist>"
+def build(phr, sht):
+    res = "\t<dict>\n"
+    res += "\t\t<string>"
+print(result+result_end)
